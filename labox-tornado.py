e5da156ad6b49873d3c30e398d8d7fedbf7b3e1c
EnsekiTT <ensekitt@gmail.com>
2016-07-09 23:18:22 +0900

    bootstrap を導入

--- labox-tornado.py
+++ labox-tornado.py
@@ -2,16 +2,18 @@ import tornado.web
 import tornado.ioloop
 import tornado.template
 import datetime
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        loader = tornado.template.Loader("./static")
-        res = loader.load("base.html").generate(message="it works!")
-        self.write(res)
+        self.render("base.html", message="hello world!")
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", MainHandler),
-    ])
+        ],
+        template_path=os.path.join(os.getcwd(), "templates"),
+        static_path=os.path.join(os.getcwd(), "static"),
+    )
     app.listen(50007)
     tornado.ioloop.IOLoop.current().start()
