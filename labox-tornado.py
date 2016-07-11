4e640cb01567f36db90c5797908b24d96d983814
EnsekiTT <ensekitt@gmail.com>
2016-07-12 01:44:17 +0900

    表現をログインからサインインにして、サインアップページを用意した。

--- labox-tornado.py
+++ labox-tornado.py
@@ -4,7 +4,9 @@ import tornado.template
 import datetime
 import os
 import settings
-import time
+import sqlite3
+import json
+
 
 class Responder():
     def __init__(self):
@@ -13,11 +15,32 @@ class Responder():
 
     def set_name(self, name):
         self.name = name
-        self.logined_info = self.name + ", " + self.first_info
+        self.signined_info = self.name + ", " + self.first_info
 
     def message_text(self, message):
         message = tornado.escape.xhtml_escape(message)
         self.response.append("えっ？ " + message + "！？")
+
+class DatabaseExample():
+    def __init__(self):
+        self.dbPath = 'database.db'
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.dbPath)
+        self.cursorobj = connection.cursor()
+
+    def disconnect(self):
+        self.connection.close()
+
+    def _query(self, query):
+        try:
+            self.cursorobj.execute(query)
+            result = self.cursorobj.fetchall()
+            self.connection.commit()
+            return result
+        except Exception:
+            raise
+
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -39,38 +62,46 @@ class FaceHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.res.set_name(tornado.escape.xhtml_escape(self.current_user))
-        self.render("face.html", message=self.res.logined_info)
+        self.render("face.html", message=self.res.signined_info)
 
     @tornado.web.authenticated
     def post(self):
         self.res.message_text(self.get_argument("message"))
         self.render("face.html", message=self.res.response[-1])
 
-
-class LoginHandler(BaseHandler):
+class SigninHandler(BaseHandler):
     def get(self):
-        self.render("login.html")
+        self.render("signin.html")
 
     def post(self):
         self.set_secure_cookie("user", self.get_argument("user"))
         self.redirect("/")
 
-class LogoutHandler(BaseHandler):
+
+class SignoutHandler(BaseHandler):
     def get(self):
         self.clear_cookie("user")
-        self.redirect(self.get_argument("next", "/?logout=True"))
+        self.redirect(self.get_argument("next", "/?signout=True"))
+
+class SignupHandler(BaseHandler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.get_argument("")
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", MainHandler),
         (r"/face", FaceHandler),
-        (r"/login", LoginHandler),
-        (r"/logout", LogoutHandler),
+        (r"/signin", SigninHandler),
+        (r"/signout", SignoutHandler),
+        (r"/signup", SignupHandler),
         ],
         template_path=os.path.join(os.getcwd(), "templates"),
         static_path=os.path.join(os.getcwd(), "static"),
         cookie_secret=settings.cookie_secret,
-        login_url='/login',
+        login_url='/signin',
     )
     app.listen(50007)
     tornado.ioloop.IOLoop.current().start()
